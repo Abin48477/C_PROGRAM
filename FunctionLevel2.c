c0d980372ba7372bb8341b0735b27e61c1065082
Abin Ghimire <ghimireabin56@gmail.com>
2025-07-03 23:35:14 +0545

    Create FunctionLevel2.c

---
+++ FunctionLevel2.c
@@ -0,0 +1,15 @@
+//📌 Example 2: With arguments, no return
+#include <stdio.h>
+void add(int a, int b) {
+    printf("Sum = %d\n", a + b);
+}
+int main() {
+    add(5, 3);  // send values
+    return 0;
+}
+
+/*📌 Function Structure:
+returnType functionName(parameters) {
+    // function body
+   return value;
+}*/
